3b6f0e48a029feef269dc9ae2fae14cf1f48f355
Marcelo Comanduci <mcomanduci@gmail.com>
2025-01-18 20:39:34 -0300

    Challenge done

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const input = document.querySelector("#amigo");
+const listaAmigos = document.querySelector("#listaAmigos");
+const amigoSorteado = document.querySelector("#resultado");
+
+const amigos = [];
+
+function adicionarAmigo() {
+  const amigo = input.value;
+  if (amigo === "") {
+    alert("Por favor, insira um nome válido.");
+    return;
+  }
+  amigos.push(amigo);
+  input.value = "";
+  mostrarAmigos();
+}
+
+function mostrarAmigos() {
+  listaAmigos.innerHTML = "";
+  amigos.forEach((amigo) => {
+    const item = document.createElement("li");
+    item.textContent = amigo;
+    listaAmigos.appendChild(item);
+  });
+}
+
+function sortearAmigo() {
+  const sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+  listaAmigos.innerHTML = "";
+  amigoSorteado.textContent = `O amigo sorteado é: ${sorteado}`;
+}
